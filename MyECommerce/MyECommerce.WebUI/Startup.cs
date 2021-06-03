2284816173e40cf55fcd8bba1173bdc901c7bddc
Zaubawk <hpagazb@gmail.com>
2021-06-03 14:00:34 -0700

    Initial Solution Setup

---
+++ MyECommerce/MyECommerce.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyECommerce.WebUI.Startup))]
+namespace MyECommerce.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
