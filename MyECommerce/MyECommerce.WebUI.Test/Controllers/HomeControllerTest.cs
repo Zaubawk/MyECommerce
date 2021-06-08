f01cac107e22200943f325db1cc51d7b44d7e930
Zaubawk <hpagazb@gmail.com>
2021-06-07 19:27:05 -0700

    Added Tests

---
+++ MyECommerce/MyECommerce.WebUI.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyECommerce.Core.Contracts;
+using MyECommerce.Core.Models;
+using MyECommerce.Core.ViewModels;
+using MyECommerce.WebUI.Controllers;
+
+namespace MyECommerce.WebUI.Test.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCatgeoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext, productCatgeoryContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+        }
+    }
+}
---
+++ MyECommerce/MyECommerce.WebUI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyECommerce.WebUI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyECommerce.WebUI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd3210c1-497c-4574-9020-7a9f1164f0bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
